a25a48987e96b63e083c2c4b84960b01ca865d9e
chuyihuang <chuyihuang@gmail.com>
2013-10-25 17:26:06 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="zh-TW">
+<head>
+  <title>Front Works</title>
+  <meta name="description" content="content">
+  <meta name="keywords" content="content">
+  <meta name="author" content="content">
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="/assets/stylesheets/bootstrap.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="/assets/stylesheets/mystyles.css" media="screen">
+  <script src="/assets/javascripts/respond.min.js" type="text/javascript"></script>
+</head>
+<body id="home">
+  <?php include './partials/xxx.php'; ?>
+  <script src="/assets/javascripts/bootstrap.js" type="text/javascript"></script>
+</body>
+</html>
